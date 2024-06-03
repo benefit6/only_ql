8a84633a6933c2e0240836918bd555ea727f480d
benefit6 <liuwuyi651@foxmail.com>
2024-06-03 19:04:01 +0800

    upload

---
+++ linuxdoCheckin.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3  
+# -- coding: utf-8 --
+# @Time : 2024/5/31 23:05
+# -------------------------------
+# cron "0 0 8 1/1 * ?" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('linux.do签到');
+
+import os
+import time
+import random
+
+from tabulate import tabulate
+from playwright.sync_api import sync_playwright
+
+
+USERNAME = os.environ.get("linuxUSERNAME")
+PASSWORD = os.environ.get("linuxPASSWORD")
+
+
+HOME_URL = "https://linux.do/"
+
+
+class LinuxDoBrowser:
+    def __init__(self) -> None:
+        self.pw = sync_playwright().start()
+        self.browser = self.pw.firefox.launch(headless=True)
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page()
+        self.page.goto(HOME_URL)
+
+    def login(self):
+        self.page.click(".header-buttons .d-button-label")
+        time.sleep(2)
+        self.page.fill("#login-account-name", USERNAME)
+        time.sleep(2)
+        self.page.fill("#login-account-password", PASSWORD)
+        time.sleep(2)
+        self.page.click("#login-button")
+        time.sleep(30)
+        user_ele = self.page.query_selector("#current-user")
+        if not user_ele:
+            print("Login failed")
+            return False
+        else:
+            print("Check in success")
+            return True
+
+    def click_topic(self):
+        for topic in self.page.query_selector_all("#list-area .title"):
+            page = self.context.new_page()
+            page.goto(HOME_URL + topic.get_attribute("href"))
+            if random.random() < 0.02:  # 100 * 0.02 * 30 = 60
+                self.click_like(page)
+            time.sleep(10)
+            page.close()
+
+    def run(self):
+        if not self.login():
+            return
+        self.click_topic()
+        self.print_connect_info()
+
+    def click_like(self, page):
+        page.locator(".discourse-reactions-reaction-button").first.click()
+        print("Like success")
+
+    def print_connect_info(self):
+        page = self.context.new_page()
+        page.goto("https://connect.linux.do/")
+        rows = page.query_selector_all("table tr")
+
+        info = []
+
+        for row in rows:
+            cells = row.query_selector_all("td")
+            if len(cells) >= 3:
+                project = cells[0].text_content().strip()
+                current = cells[1].text_content().strip()
+                requirement = cells[2].text_content().strip()
+                info.append([project, current, requirement])
+
+        print("--------------Connect Info-----------------")
+        print(tabulate(info, headers=["项目", "当前", "要求"], tablefmt="pretty"))
+
+        page.close()
+
+
+if __name__ == "__main__":
+    if not USERNAME or not PASSWORD:
+        print("Please set USERNAME and PASSWORD")
+        exit(1)
+    l = LinuxDoBrowser()
+    l.run()
